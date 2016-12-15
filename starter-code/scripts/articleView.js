9f5ce738eb26e34a0b783596fc2ea953c9f30e19
Brigitte Huneke <brigittehuneke@gmail.com>
2016-12-14 20:51:29 -0800

    got the hide/fade in features to work for the authors and categories

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -16,46 +16,55 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.populateFilters();
-
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
-  if ($(this).val()) {
-    $('#articles').hide();
+    if ($(this).val()) {
       /* TODO: If the slect box changes to an option that has a value, we should:
           1. Hide all of the articles
           2. Fade in only the articles that match based on on the author
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
-        $(this).parents('#articles').fadeIn(6000);
-  } else {
+      $('article').hide();
+      var authorName = $(this).val();
+      $('article[data-attribute = "' + authorName + '"]').fadeIn();
+    } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
-        $('#articles').not('.template').show();
-  }
+      $('article').not('.template').show();
+    }
     $('#category-filter').val('');
   });
 };
-
-articleView.handleAuthorFilter();
 
 articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function(){
+    if ($(this).val()) {
+      $('article').hide();
+      var categoryName = $(this).val();
+      $('article[data-category = "' + categoryName + '"]').fadeIn();
+    } else {
+      $('article').not('.template').show();
+    }
+    $('#author-filter').val('');
+  });
 };
 
-articleView.handleMainNav = function () {
-  $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
-      1. Hide all of the .tab-content sections
-      2. Fade in the single .tab-content section that is
-        associated with the .tab element's data-content attribute.
-    */
-  });
-  $('.main-nav .tab:first').click();
-};
+// articleView.handleMainNav = function () {
+//   $('.main-nav').on('click', '.tab', function() {
+//     /* TODO:
+//       1. Hide all of the .tab-content sections
+//       2. Fade in the single .tab-content section that is
+//         associated with the .tab element's data-content attribute.
+//     */
+//     //$('.tab-content').hide();
+//     //$('li.tab').
+//   //});
+//   //$('.main-nav .tab:first').click();
+// };
 
 articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
@@ -70,3 +79,8 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+//articleView.handleMainNav();
+//articleView.setTeasers();
